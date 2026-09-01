d3f31fd144c1826f3ae9ed6068603485a7b2d03e
CHristina <christina.schnorr@gmx.de>
2026-09-01 08:03:40 +0200

    data for dishes added

---
+++ dishes.js
@@ -0,0 +1,173 @@
+let starters = [
+    {
+        "name": "Bruschetta",
+        "description": "gewürzte Tomatenwürfel auf leckerem Baguette",
+        "price": 6.95,
+        "inBasket": false,
+    },
+    {
+        "name": "Carpaccio",
+        "description": "zartes Beef mit hochwertigem Olivenöl gewürzt",
+        "price": 9.95,
+        "inBasket": false,
+    },
+    {
+        "name": "Vitello Tonnato",
+        "description": "zartes Beef mit leckerer Tuna-Creme & Kapern",
+        "price": 9.95,
+        "inBasket": false,
+    },
+    {
+        "name": "gemischter Beilagensalat",
+        "description": "knackiger Salat mit Honig-Senf-Dressing",
+        "price": 7.95,
+        "inBasket": false,
+    }
+
+];
+
+let mains = [
+    pizza = [
+        {
+            "name": "Margherita",
+            "description": "frische Tomatensauce, Mozzarella",
+            "price": 8.95,
+            "inBasket": false,
+        },
+        {
+            "name": "Tonno",
+            "description": "frische Tomatensauce, Mozzarella, Thunfisch, Zwiebeln",
+            "price": 11.95,
+            "inBasket": false,
+        },
+        {
+            "name": "Hawaii",
+            "description": "frische Tomatensauce, Mozzarella, Schinken, Ananas",
+            "price": 11.95,
+            "inBasket": false,
+        },
+        {
+            "name": "Capriciosa",
+            "description": "frische Tomatensauce, Mozzarella, Schinken, Pilze, Thunfisch",
+            "price": 12.95,
+            "inBasket": false,
+        }
+
+    ],
+
+    pasta = [
+        {
+            "name": "Pomodoro",
+            "description": "Penne, frische Tomatensauce, Parmesan",
+            "price": 9.95,
+            "inBasket": false,
+        },
+        {
+            "name": "Bolognese",
+            "description": "Spaghetti, frische Hackfleischsauce, Parmesan",
+            "price": 9.95,
+            "inBasket": false,
+        },
+        {
+            "name": "Carbonara",
+            "description": "Spaghetti, Sahnesauce, Schinken, Parmesan",
+            "price": 9.95,
+            "inBasket": false,
+        },
+    ],
+
+    burger = [
+        {
+            "name": "All meat burger",
+            "description": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ sauce",
+            "price": 15.95,
+            "inBasket": false,
+        },
+        {
+            "name": "Beef red burger",
+            "description": "Beef, Cheese, Tomatoes, Lettuce, Onion",
+            "price": 14.95,
+            "inBasket": false,
+        },
+        {
+            "name": "Big chicken burger",
+            "description": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
+            "price": 14.95,
+            "inBasket": false,
+        }
+    ],
+
+    salad = [
+        {
+            "name": "Warm beef arugula salad",
+            "description": "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
+            "price": 15.95,
+            "inBasket": false,
+        },
+        {
+            "name": "Ceasar Salad",
+            "description": "knackiger Salat mit zartem Hähnchen & Parmesan",
+            "price": 10.95,
+            "inBasket": false,
+        },
+        {
+            "name": "Green salad with seafood",
+            "description": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+            "price": 14.95,
+            "inBasket": false,
+        },
+        {
+            "name": "Vegan green salad with tofu",
+            "description": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+            "price": 14.95,
+            "inBasket": false,
+        }
+    ]
+
+];
+
+let dessert = [
+    {
+        "name": "Tiramisu",
+        "price": 7.95,
+        "inBasket": false,
+    },
+    {
+        "name": "American Cheesecake",
+        "price": 5.95,
+        "inBasket": false,
+    },
+    {
+        "name": "Mascarpone mit Erdbeersauce",
+        "price": 6.95,
+        "inBasket": false,
+    },
+    {
+        "name": "Carrot Cake",
+        "price": 3.95,
+        "inBasket": false,
+    },
+];
+
+let sides = [
+    {
+        "name": "Pommes",
+        "price": 3.95,
+        "inBasket": false,
+    },
+    {
+        "name": "Süßkartoffelpommes",
+        "price": 4.95,
+        "inBasket": false,
+    },
+    {
+        "name": "Kartoffelspalten",
+        "price": 3.95,
+        "inBasket": false,
+    },
+    {
+        "name": "Gemüsesticks",
+        "price": 3.95,
+        "inBasket": false,
+    },
+]
